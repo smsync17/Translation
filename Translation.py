6c152bd4423e788d6db155fb18cfd0fca860e359
q44302sm <q44302sm@student.manchester.ac.uk>
2026-08-09 23:28:14 +0100

    connected Whisper, which Transcripts the audio into text

--- Translation.py
+++ Translation.py
@@ -1,1 +1,12 @@
-print("Hello World")
+import whisper
+
+# Load the model
+model = whisper.load_model("base")
+
+# Transcribe test file
+result = model.transcribe("C:/Users/SM_Ga/Documents/Sound Recordings/Test_6.m4a", language="de")
+# language will transcript different languages, "de" = german, "ar" = arabic, "it" = italian, etc.
+# translate is meant to translate it back to english, but it kinda sucks
+
+# Print transcription
+print(result["text"])
